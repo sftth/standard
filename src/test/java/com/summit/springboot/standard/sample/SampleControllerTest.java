796f8826fca3486fa163277f874ab2775d810ee3
sftth <sftth318@gmail.com>
2019-08-27 00:46:14 +0900

    4부 테스트 WebTestClient 활용 예제

--- src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
+++ src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -20,13 +20,16 @@ public class SampleControllerTest {
     @MockBean
     SampleService mockSampleService;
 
+    @Autowired
+    WebTestClient webTestClient;
+
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("Jesus");
 
-
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello Jesus");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello Jesus");
 
     }
 }
