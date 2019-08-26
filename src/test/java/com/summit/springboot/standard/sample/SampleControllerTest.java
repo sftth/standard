6df48df46d7301c8f97301d28acaeff3f56050da
sftth <sftth318@gmail.com>
2019-08-27 00:27:52 +0900

    4부 테스트 @MockBean 활용으로 테스트 범위 외 객체를 mock 객체로 대체

--- src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
+++ src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,10 +17,16 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("Jesus");
+
+
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello Jacob");
+        assertThat(result).isEqualTo("hello Jesus");
 
     }
 }
