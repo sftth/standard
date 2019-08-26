0b74aed33573205413c3c0d0d8059d70eb95ee24
sftth <sftth318@gmail.com>
2019-08-27 00:54:13 +0900

    4부 테스트 @WebMvcTest 활용

--- src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
+++ src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
@@ -3,33 +3,29 @@ package com.summit.springboot.standard.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
-    @Autowired
-    TestRestTemplate testRestTemplate;
-
     @MockBean
     SampleService mockSampleService;
 
     @Autowired
-    WebTestClient webTestClient;
+    MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("Jesus");
 
-        webTestClient.get().uri("/hello").exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello Jesus");
-
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("hello Jesus"));
     }
 }
