728b58a9c6872ea49eeba682f7ac5b70c441e061
sftth <sftth318@gmail.com>
2019-08-27 16:26:25 +0900

    스프링 웹 MVC HttpMessageConverters

---
+++ src/main/java/com/summit/springboot/standard/user/UserController.java
@@ -0,0 +1,16 @@
+package com.summit.springboot.standard.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
+    }
+}
