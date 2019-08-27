b0dc897b2ca6eecb10bd9c70a1e17e5e62598443
sftth <sftth318@gmail.com>
2019-08-27 13:13:31 +0900

    4부 스프링 부트 활용 - 테스트 유틸

--- src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
+++ src/test/java/com/summit/springboot/standard/sample/SampleControllerTest.java
@@ -1,13 +1,16 @@
 package com.summit.springboot.standard.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,6 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @MockBean
     SampleService mockSampleService;
 
@@ -27,5 +33,7 @@ public class SampleControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello Jesus"));
+
+        assertThat(outputCapture.toString()).contains("=== Log Test ===").contains("++++ Log Test +++");
     }
 }
