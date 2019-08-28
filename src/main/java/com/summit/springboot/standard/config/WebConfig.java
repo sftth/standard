7b9699d799a78082ad7da014b14ec21f7f22edc1
sftth <sftth318@gmail.com>
2019-08-29 01:02:59 +0900

    스프링 웹 MVC HttpMessageConverters

---
+++ src/main/java/com/summit/springboot/standard/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.summit.springboot.standard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCachePeriod(20);
+    }
+}
